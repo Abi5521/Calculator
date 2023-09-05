74ce336322c8c02c4f34346a98905ede84aac738
Abi5521 <138142782+Abi5521@users.noreply.github.com>
2023-09-05 15:03:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function Addition(){
+    var a = document.getElementById("i1").value;
+    var b = document.getElementById("i2").value;
+    c = parseInt(a)+parseInt(b);
+    document.getElementById("result").innerHTML = c;
+}
+function Subtraction(){
+    var a = document.getElementById("i1").value;
+    var b = document.getElementById("i2").value;
+    var c = a-b;
+    document.getElementById("result").innerHTML = c;
+}
+function Multiplication(){
+    var a = document.getElementById("i1").value;
+    var b = document.getElementById("i2").value;
+    var c = a*b;
+    document.getElementById("result").innerHTML = c;
+}
+function Division(){
+    var a = document.getElementById("i1").value;
+    var b = document.getElementById("i2").value;
+    var c = a/b;
+    document.getElementById("result").innerHTML = c;
+}
